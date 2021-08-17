21999ec7d0c70602b8481d8c91ec62a85703d62e
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-18 07:35:34 +1000

    Lab completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(arr)
+    matchings = []
+    arr.each do |word| 
+      word.split("").sort == @word.split("").sort && matchings << word
+    end
+    matchings
+  end
+end
+
